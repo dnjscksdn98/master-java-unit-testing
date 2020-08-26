46ef60997d07234af556d6964ac696c5cb1fc4df
Alex Won <dnjscksdn98@gmail.com>
2020-08-26 20:47:12 +0900

    initial commit

---
+++ src/test/java/com/udemy/SpringBootUnitTest/SpringBootUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.SpringBootUnitTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
