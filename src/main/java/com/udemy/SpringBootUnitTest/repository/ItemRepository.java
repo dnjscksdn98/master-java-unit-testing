58e99b8d4d1a5bdba1e76ab8d7456ca3ae7fd21c
Alex Won <dnjscksdn98@gmail.com>
2020-08-28 22:58:20 +0900

    feat: create data layer for item

---
+++ src/main/java/com/udemy/SpringBootUnitTest/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.SpringBootUnitTest.repository;
+
+import com.udemy.SpringBootUnitTest.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
