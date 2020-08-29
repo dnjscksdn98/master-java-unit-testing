690f533de385e1d042bf319690a9114fa501559c
Alex Won <dnjscksdn98@gmail.com>
2020-08-29 11:29:08 +0900

    test: write controller, service, data tests

--- src/test/java/com/udemy/SpringBootUnitTest/controllers/ItemControllerTests.java
+++ src/test/java/com/udemy/SpringBootUnitTest/controllers/ItemControllerTests.java
@@ -13,6 +13,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,5 +61,26 @@ class ItemControllerTests {
 
         String actualResponse = "{\"id\":1,\"name\":\"Mac Book Pro\",\"price\":10,\"quantity\":100}";
         JSONAssert.assertEquals(result.getResponse().getContentAsString(), actualResponse, true);
+    }
+
+    @Test
+    public void list() throws Exception {
+        when(itemService.findAll())
+                .thenReturn(List.of(
+                        new Item(1L, "Mac Book Pro", 10, 100),
+                        new Item(2L, "IPhone", 5, 70),
+                        new Item(3L, "IPad", 7, 90)));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/items")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String expectedResponse = "[{id:1,name:\"Mac Book Pro\",price:10,quantity:100},{id:2,name:\"IPhone\",price:5,quantity:70},{id:3,name:\"IPad\",price:7,quantity:90}]";
+        JSONAssert.assertEquals(expectedResponse, result.getResponse().getContentAsString(), false);
     }
 }
