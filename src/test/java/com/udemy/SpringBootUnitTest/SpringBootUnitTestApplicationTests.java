9d2083cbf5279fd363975861b7c8650c5cc088ad
Alex Won <dnjscksdn98@gmail.com>
2020-09-05 22:34:34 +0900

    feat: set up different test configurations

--- src/test/java/com/udemy/SpringBootUnitTest/SpringBootUnitTestApplicationTests.java
+++ src/test/java/com/udemy/SpringBootUnitTest/SpringBootUnitTestApplicationTests.java
@@ -2,8 +2,10 @@ package com.udemy.SpringBootUnitTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:test-configuration.yml"})
 class SpringBootUnitTestApplicationTests {
 
 	@Test
